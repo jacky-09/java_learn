70831178877ed21386501cf4cbf5b63fa232d471
Jacky <62315357+jackyfzh@users.noreply.github.com>
2021-01-25 17:11:36 +0800

    Create d2.java

---
+++ d2.java
@@ -0,0 +1,18 @@
+public class d2 {
+    public static void main(String[] args) {
+        // 定义变量
+        int a = 11; // int整数
+        String b = "Hello Guy"; // String字符串
+        float c = 3.14159f; // float浮点型
+        boolean d = 18 < a; // boolean布尔值（F）
+        char e = 'A'; // char单字符
+        boolean f = a < 18; // boolean布尔值（T)
+        // 打印变量
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+    }
+}
