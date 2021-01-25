65eb4e54ca2cc0bebfbf454fcde67fab6ce9f1aa
Jacky <62315357+jackyfzh@users.noreply.github.com>
2021-01-25 17:12:08 +0800

    Create d3.java

---
+++ d3.java
@@ -0,0 +1,13 @@
+public class d3 {
+    public static void main(String[] args) {
+        // 整数运算
+        int n1 = 100; // 数字一
+        int n2 = 25; // 数字二
+        int num_p = n1 + n2; // 加法计算结果
+        System.out.println(num_p); // 打印结果
+        // 布尔运算（primary student的定义: 6~12岁）
+        int age = 15; // 定义年龄
+        boolean isPrimaryStudent = age > 6 && age < 12; // 设定条件
+        System.out.println(isPrimaryStudent ? "是" : "否"); // 是或否的打印内容
+    }
+}
